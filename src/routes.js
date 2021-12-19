bddd21656eb00ca522c2258d71f1fdaa33b5272c
joao <jgerhardt923@gmail.com>
2021-12-19 17:35:58 -0400

    routes.js atualizado

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,16 @@
 const express = require("express");
 
+const loginController = require("./controller/login")
+const userController = require("./controller/user")
+
 const router = express.Router();
+
+router.route("/login")
+    .post(loginController.post)
+
+router.unsubscribe("/user")
+    .get(userController.get)
+    .post(userController.post)
 
 
 module.exports = router;
