1bd1d8229956725b6400c47f77b656ee834ec508
joao <jgerhardt923@gmail.com>
2021-12-19 18:20:33 -0400

    sessionMidlleware criado

---
+++ src/midlleware/session.js
@@ -0,0 +1,22 @@
+const config = require("../config")
+const session = require("../model/session");
+const user = require("../model/user");
+
+module.exports = async(req, res, next)=>{
+    if(req.cookies.sessionId){
+        let sess = await session.findOne({where:{id:req.cookies.sessionId}})
+        if(Date.now() >= sess.expire){
+            next();
+        }else{
+            let usr = await user.findOne({where:{id:sess.userId}})
+            req.user = await usr
+            await next()
+        }
+    }else{
+        if(req.path === "/login"){
+            next();
+        }else{
+            res.sendFile(config.app.staticDir + "/templates/login.html");
+        }
+    }
+};
