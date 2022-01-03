94bba730453efd80ce0784c69fdad4c56bb21668
joao <jgerhardt923@gmail.com>
2022-01-02 21:51:49 -0400

    interface de usuario comum criada

--- src/public/static/js/index.js
+++ src/public/static/js/index.js
@@ -5,50 +5,82 @@ var app = new Vue(
             shippingcompany:{
                 data:[],
                 _data:[],
+                $data:[],
                 set data(value){
                     data = value;
-                    this._data = data
+                    this._data = data;
+                    this.$data = data;
                 }
             },
             driver:{
                 data:[],
                 _data:[],
+                $data:[],
                 set data(value){
                     data = value;
-                    this._data = data
+                    this._data = data;
+                    this.$data = data;
                 }
             },
             truck:{
                 data:[],
                 _data:[],
+                $data:[],
                 set data(value){
                     data = value;
-                    this._data = data
+                    this._data = data;
+                    this.$data = data;
                 }
             },
             local:{
                 data:[],
                 _data:[],
+                $data:[],
                 set data(value){
                     data = value;
-                    this._data = data
+                    this._data = data;
+                    this.$data = data;
+                }
+            },
+            association:{
+                data:[],
+                _data:[],
+                $data:[],
+                set data(value){
+                    data = value;
+                    this._data = data;
+                    this.$data = data;
+                }
+            },
+            freight:{
+                data:[],
+                _data:[],
+                $data:[],
+                set data(value){
+                    data = value;
+                    this._data = data;
+                    this.$data = data;
                 }
             }
-        },
-        watch:{
-            shippingcompany:function (){
-                this.shippingcompany._data = this.shippingcompany.data;
+            ,
+            travel:{
+                data:[],
+                _data:[],
+                $data:[],
+                set data(value){
+                    data = value;
+                    this._data = data;
+                    this.$data = data;
+                }
             }
         },
         methods:{
             fetchListData: function(model){
                 let url = "http://localhost:5000/"+model+"/list/0";
-                console.log("it works "+url);
                 $.ajax({
                     url: url,
                     method:"GET",
                     success:data=>{
-                        console.log(data);
                         this[model].data = data.data;
                     }
                 })
@@ -57,16 +89,59 @@ var app = new Vue(
                 let $form = $(form);
                 let model = form.replace("Form","").replace("#","");
                 let url = "http://localhost:5000/"+model+"/add/0";
-                console.log("it works "+url);
                 $.ajax({
                     url: url,
                     method:"POST",
                     data:getFormData($form),
                     success:data=>{
-                        console.log(data);
                         this[model].data = data.data;
                     }
-                }).done(data=>{this.fetchListData(model);$form.reset()})
+                }).done(data=>{this.fetchListData(model)})
+            },
+            updateData:function(form, id){
+                let $form = $(form);
+                let model = form.replace("UpdateForm","").replace(/[0-9]/g,"").replace("#","");
+                let url = "http://localhost:5000/"+model+"/update/"+id;
+                $.ajax({
+                    url: url,
+                    method:"PUT",
+                    data:getFormData($form),
+                    success:data=>{
+                        this[model].data = data.data;
+                    }
+                }).done(data=>{this.fetchListData(model)})
+            },
+            deleteObject:function(model, id){
+                let confirm = window.confirm("do you realy want to delete it?");
+                if (confirm == true){
+                    let url = "http://localhost:5000/"+model+"/del/"+id;
+                    $.ajax({
+                        url: url,
+                        method:"DELETE",
+                        data:{id:id},
+                        success:data=>{
+                            alert("Deleted!")
+                        }
+                    }).done(data=>{this.fetchListData(model)})
+                }
+            },
+            rowToForm:function(tBody, rowId){
+                let body = document.querySelector("#"+tBody)
+                let row = body.querySelector("#"+rowId);
+                let modCels = row.querySelectorAll(".tdValue");
+                modCels.forEach(value=>{
+                    value.innerHTML = `<input type="text" value="${value.innerHTML}" name="${value.id}" id="${value.id}">`
+                });
+            },
+            toggle:function(elementSelector, mili){
+                $(elementSelector).toggle(mili);
+            },
+            showHide: function(hide, show){
+                let divs = document.getElementsByClassName(hide);
+                for (div of divs){
+                    div.style.display = "none";
+                }
+                document.querySelector(show).style.display = "block";
             }
         }
     }
