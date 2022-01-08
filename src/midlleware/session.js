09f15a3c2602f6f1d9bc91077142fabac23a6adf
joao <jgerhardt923@gmail.com>
2022-01-08 13:15:44 -0400

    midlleware de sessao atualizado

--- src/midlleware/session.js
+++ src/midlleware/session.js
@@ -5,12 +5,17 @@ const user = require("../model/user");
 module.exports = async(req, res, next)=>{
     if(req.cookies.sessionId){
         let sess = await session.findOne({where:{id:req.cookies.sessionId}})
-        if(Date.now() >= sess.expire){
-            next();
+        if(sess !== null | undefined){
+            if(Date.now() >= sess.expire){
+                
+                next();
+            }else{
+                let usr = await user.findOne({where:{id:sess.userId}})
+                req.user = await usr
+                await next()
+            }
         }else{
-            let usr = await user.findOne({where:{id:sess.userId}})
-            req.user = await usr
-            await next()
+            res.redirect("/login");
         }
     }else{
         if(req.path === "/login"){
