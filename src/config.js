4e82b5923a9dbb379f7ef24a2896560bae7e9789
joao <jgerhardt923@gmail.com>
2022-01-08 13:53:28 -0400

    server config updated

--- src/config.js
+++ src/config.js
@@ -8,7 +8,7 @@ module.exports = {
         name: process.env.DB_NAME || "grupoTransportesDiamantino",
         user: process.env.DB_USER || "postgres",
         password: process.env.DB_PASSWORD,
-        host: process.env.DATABASE_URL || "localhost",
+        host: process.env.DB_HOST || "localhost",
         port: process.env.DB_PORT || 5432
     },
     superUser:{
