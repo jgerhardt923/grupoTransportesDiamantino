5e3a58b29b60d8799cb0956a20fb503176357354
joao <jgerhardt923@gmail.com>
2022-01-08 13:39:44 -0400

    server config updated

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const config = require("./src/config")
 const app = require("./src/app");
 
+try{
+    require("./src/model/dataBase/createSuperUser");
+}catch (err){
+    console.log(err)
+}
+
 app.listen(config.server.port, ()=>{
     console.log(`runing on ${config.server.port}`);
 });
